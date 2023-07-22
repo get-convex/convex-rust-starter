10e668431ecc1acc38a873228cebe0a33012f22d
Ian Macartney <ianhmacartney@gmail.com>
2023-07-21 20:23:08 -0700

    1.0

--- convex/messages.ts
+++ convex/messages.ts
@@ -2,18 +2,14 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 
 export const add = mutation({
-  args: {
-    author: v.string(),
-    body: v.string(),
-  },
-  handler: async ({ db }, { author, body }) => {
-    await db.insert("messages", {
-      author,
-      body,
-    });
+  args: { author: v.string(), body: v.string() },
+  handler: async (ctx, { author, body }) => {
+    await ctx.db.insert("messages", { author, body });
   },
 });
 
-export const collect = query(async ({ db }) => {
-  return await db.query("messages").collect();
+export const collect = query({
+  handler:async (ctx) => {
+  return await ctx.db.query("messages").collect();
+}
 });
